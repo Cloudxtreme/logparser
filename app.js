f9494d4f4460eddb0204ec1494419f8ed058e3b7
tomarcafe <angelo.tomarcafe@gmail.com>
2014-10-05 10:51:34 +0700

    now does not store results in RAM for nginxParser (instead count them in-place, as they go, and discard)
    now does store all errorous log entries (so one could analyze them later). probably this leads to much verbose output. probably those should be grouped/counted by domain that produces this bad entry (anyways any help of collecting errors i see - is notify user that he uses jsdelivr wrong)

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ var filename = 'resources/sample logs/maxcdn/sample log maxcdn.txt';
 parser.parseMaxcdn(filename, function(ret){
     var bads = ret[0];
     var stats = ret[1];
-    console.log("1. List of entries which could not be counted: ");
-    console.log(bads);
-    console.log();
-    console.log();
-    console.log("2. Staticstics for log file '" + filename + "'");
+ //   console.log("1. List of entries which could not be counted: ");
+   // console.log(bads);
+   // console.log();
+   // console.log();
+    console.log("1. Staticstics for log file '" + filename + "'");
     console.log(stats);
   //  console.log ('strings parsed: '+ret);
 });
--- prsr.js
+++ prsr.js
@@ -30,45 +30,74 @@ testThrowError = false;
 if (testThrowError) {    testErr();     }
 // --- unit end. ---
 
-
+//localizing needed PARSER' properties
 var PARSER = utils.PARSER;
 
 var tryMatcherObject = PARSER.tryMatcherObject;
 var tryMatchingFunction = PARSER.tryMatchingFunction;
 
 var endMatcher = PARSER.storeNginx.responseCodeMatcher;
-
 var testQuote = PARSER.storeNginx.testQuote;
+
 var testNewline = PARSER.storeMaxcdn.testNewline;
+
+//return position of next log item start
 var nextStartNginx = function(buffer, position) {
     while (buffer.length >= position++ && !tryMatchingFunction(buffer, position, testQuote)){
     }
     return position;
 };
 
+//return position of next log item start
 var nextStartMaxcdn = function(buffer, position) {
     while (buffer.length >= position++ && !tryMatchingFunction(buffer, position, testNewline)){
     }
     return position;
 };
 
+/**
+ * replacements for _.countBy that allows instant counting,
+ * without need to store all data in ram berfore count
+ * @param statsKey -what property inside stats object is handled
+ * @param itemProp - value of that property for currently scanned item
+ * @param stats - object that holds counters for parsed file
+ * @private
+ */
+// TODO: namespacing
+var __count = function(statsKey, itemProp, stats) { //todo: move to
+    if (!stats[statsKey]) stats[statsKey] = {};
+    if (itemProp) {
+        if (!stats[statsKey][itemProp]) {
+            stats[statsKey][itemProp] = 0;
+        }
+        stats[statsKey][itemProp]++;
+    }
+};
+
 var parseNginx = function(filename, callback) {
-    var bads = [];
-    var stats = { 'By project': '',
+
+    var bads = []; //here be
+    var stats = { 'By project': {}, 'By response code': {}, 'By protocol': {}, 'By method': {},
         'GETs count': 0, 'HEADs count': 0, 'Errors count': 0, 'Multiple queries': 0, 'Single queries': 0
     };
 
     function _check(item, str) {
         if (!item.key || !item.method || !item.response || !item.protocol) {
-            bads.push({ str: str.split('?')[0], item: item});
+            bads.push({ str: str.split('?')[0], item: item, meta:"undefined key or method or response or protocol"});
         }
+    }
+
+    function _count(item){
+        __count('By project', item.key, stats);
+        __count('By response code', item.response, stats);
+        __count('By protocol', item.protocol, stats);
+        __count('By method', item.method, stats);
     }
 
     fs.readFile(filename, function (err, file) {
         // check if file read successful
         if (err) { callback([err, err]); return;}
 
-        var results = []; //here we store :)
         var inside = false, prev, next = 0; //buffer parsing finite-state-automate
 
         do {
@@ -95,7 +124,6 @@ var parseNginx = function(filename, callback) {
 
                     var str = file.toString('ascii', next, fin + 5);
                     var string = str.split(' ');
-                    //   console.log(string );//= file.toString('ascii', next, fin+5).split(' ');
                     var q = {
                         method: string[0].replace('"', ''),
                         protocol: string[2].replace('"', ''),
@@ -103,6 +131,7 @@ var parseNginx = function(filename, callback) {
                     };
 
                     string = string[1].split('?')[0];
+                    //multiple query
                     if (string.indexOf('/g/') == 0) {
                         stats['Multiple queries']++;
                         string = string.split('/g/')[1].split(',');
@@ -111,39 +140,28 @@ var parseNginx = function(filename, callback) {
                             r.push(_.extend({}, q, {key: s.split('@')[0]}));
                         });
                         _.each(r, function (item) {
-                            results.push(item);
                             _check(item, str);
+                            _count(item);
                         })
+                        //single query
                     } else {
                         stats['Single queries']++;
                         string = string.split('/')[1];
                         q.key = string;
-                        results.push(q);
                         _check(q, str);
+                        _count(q);
                     }
                 }
                 inside = false;
             }
         } while (prev != next && tryMatchingFunction(file, next, testQuote));
 
-        stats['By project'] = JSON.stringify(_.countBy(results, function (item) {
-            return item.key;
-        }));
-        stats['By response code'] = _.countBy(results, function (item) {
-            return item.response;
-        });
-        stats['By protocol'] = _.countBy(results, function (item) {
-            return item.protocol;
-        });
-        stats['By method'] = _.countBy(results, function (item) {
-            return item.method;
-        });
+        stats['By project'] = JSON.stringify(stats['By project']);
         stats['Bad entries by log string'] = _.countBy(bads, 'str');
         callback([bads, stats]); //TODO: define some stable logical interface here
     });
 
 };
-
 
 var parseMaxcdn = function(filename, callback) {
 
@@ -160,89 +178,69 @@ var parseMaxcdn = function(filename, callback) {
     }
 
     // so we do not need to store 1M records only to use _.countBy on them..
-    // TODO: apply same with nginx log [arser
     function _count(item){
-
-        if (item.key) {
-            if (!stats['By project'][item.key]) {
-                stats['By project'][item.key] = 1;
-            } else {
-                stats['By project'][item.key]++;
-            }
-        }
-
-        if (item.response) {
-            if (!stats['By response code'][item.response]) {
-                stats['By response code'][item.response] = 1;
-            } else {
-                stats['By response code'][item.response]++;
-            }
-        }
+        if (item.key
+            && item.key.indexOf("&") == -1
+            && item.key.indexOf("+") == -1
+            && item.key.indexOf("=") == -1 )
+            __count('By project', item.key, stats);
+        __count('By response code', item.response, stats);
     }
 
     fs.readFile(filename, function (err, file) {
         // check if file read successful
         if (err) { callback([err, err]); return;}
 
-        var results = []; //here we store :)
-        var inside = false, prev, next = -1, counter=0; //buffer parsing finite-state-automate
+        var prev, next = -1; //buffer parsing finite-state-automate
 
         do {
-            prev = next;
+            prev = next; //those are positions inside buffer
             next = nextStartMaxcdn(file, prev + 1);
-            var str = file.toString  ('ascii', prev+1, next);
+            var str = file.toString  ('ascii', prev+1, next); //cut between 'prev' and next item start
+            //log item format is tab-separated values
             var string = str.split('	');
-            counter++;
             if (string.length < 8) {
                 console.log(str);
-                bads.push(str);
+                bads.push({str: str, meta: "item split by tabs contain less <8 parts"});
                 continue;
             }
 
             var q = {
-              //  querydomain: string[0],
-              //  from: string[1],
-              //  date: string[2],
+              //  querydomain: string[0],              //  from: string[1],              //  date: string[2],
                 path: string[3],
-                response: string[4],
-              //  length: string[5],
-              //  domain: string[6],
-              //  client: string[7]
+                response: string[4]
+              //  length: string[5],              //  domain: string[6],              //  client: string[7]
             };
 
-            string = q.path;
-            if (!string ) {
-                bads.push(str);
-            }
-            if (string && string.indexOf('/g/') == 0) {
-                stats['Multiple queries']++;
-                string = string.split('/g/')[1].split(',');
-                r = [];
-                _.each(string, function (s) {
-                    r.push(_.extend({}, q, {key: s.split('@')[0]}));
-                });
-                _.each(r, function (item) {
+            string = q.path; // /g/<projects,> or /<project>
+            if (!string ) { bads.push({str: str, meta:"could not extract path"}); }
+            else {
+                //multiple
+                if (string && string.indexOf('/g/') == 0) {
+                    stats['Multiple queries']++;
+                    string = string.split('/g/')[1].split(',');
+                    r = [];
+                    _.each(string, function (s) {
+                        r.push(_.extend({}, q, {key: s.split('@')[0]}));
+                    });
+                    _.each(r, function (item) {
 
-                   // results.push(item);
-                    _check(item, q.path, str);
-                    _count(item);
-                })
-            } else {
-                stats['Single queries']++;
-                string = string.split('/')[1];
-                q.key = string;
-               // results.push(q);
-                _check(q, q.path, str);
-                _count(q);
+                        _check(item, str);
+                        _count(item);
+                    });
+                //single
+                } else {
+                    stats['Single queries']++;
+                    string = string.split('/')[1];
+                    q.key = string;
+                    _check(q, str);
+                    _count(q);
+                }
             }
-
         } while (prev != next && tryMatchingFunction(file, next, testNewline));
 
         stats['By project'] = JSON.stringify(stats['By project']);
-       /*) stats['By response code'] = _.countBy(results, function (item) {
-            return item.response;
-        });*/
-        stats['Bad entries by log string'] = _.countBy(bads, 'str');
+        stats['Bad entries by log string'] =  _.countBy(bads, 'str');
         callback([bads, stats]/*[bads, stats]*/); //TODO: define some stable logical interface here
     });
 
@@ -252,99 +250,3 @@ module.exports = {
     parseNginx: parseNginx,
     parseMaxcdn: parseMaxcdn
 };
-
-/*
- var parseMaxcdn = function(filename, callback) {
-
- var bads = [];
- var stats = { 'By project': '',
- 'GETs count': 0, 'HEADs count': 0, 'Errors count': 0, 'Multiple queries': 0, 'Single queries': 0
- };
-
- function _check(item, str) {
- if (!item.key || !item.response ) {
- bads.push({ str: str.split('?')[0], item: item});
- }
- }
-
- function _count(item){
-
- if (item.key) {
- if (!stats['By project'][item.key]) {
- stats['By project'][item.key] = 1;
- } else {
- stats['By project'][item.key]++;
- }
- }
-
- if (item.response) {
- if (!stats['By response code'][item.response]) {
- stats['By response code'][item.response] = 1;
- } else {
- stats['By response code'][item.response]++;
- }
- }
- }
-
- fs.readFile(filename, function (err, file) {
- // check if file read successful
- if (err) { callback([err, err]); return;}
-
- var results = []; //here we store :)
- var inside = false, prev, next = -1, counter=0; //buffer parsing finite-state-automate
-
- do {
- prev = next;
- next = nextStartMaxcdn(file, prev + 1);
- var str = file.toString  ('ascii', prev+1, next);
- var string = str.split('	');
- var q = {};
-
- if (string.length !=8 ) {
- bads.push(str);
- } else {
- q = {
- //  querydomain: string[0],
- //  from: string[1],
- //  date: string[2],
- path: string[3],
- response: string[4],
- //  length: string[5],
- //  domain: string[6],
- //  client: string[7]
- };
-
- string = q.path;
- if (string && string.indexOf('/g/') == 0) {
- stats['Multiple queries']++;
- string = string.split('/g/')[1].split(',');
- r = [];
- _.each(string, function (s) {
- r.push(_.extend({}, q, {key: s.split('@')[0]}));
- });
- _.each(r, function (item) {
- _count(item);
- //results.push(item);
- _check(item, q.path);
- })
- } else {
- stats['Single queries']++;
- string = string.split('/')[1];
- q.key = string;
- _count(q);
- //results.push(q);
- _check(q, q.path);
- }
- }
- counter++;
-
-
- } while (prev != next && tryMatchingFunction(file, next, testNewline));
-
- stats['By project'] = JSON.stringify(stats['By project']);
- stats['Bad entries by log string'] = _.countBy(bads, 'str');
- callback([bads, stats]); //TODO: define some stable logical interface here
-});
-
-};
- */
